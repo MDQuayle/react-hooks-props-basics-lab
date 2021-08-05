8cb91a1ad14bbf0dc8c82f0b19b7c7abf35f6782
mdquayle <misternopants@gmail.com>
2021-08-04 20:13:58 -0400

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,33 @@
+import { data } from "browserslist";
 import React from "react";
-
-function About() {
+import Links from "./Links"
+import user from "../data/user"
+function About(props) {
+  if (!props.bio){
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links 
+        github = {user.links.github}
+        linkedin = {user.links.linkedin}
+        />
+      </div>
   );
+              }
+    else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{props.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links 
+        github = {user.links.github}
+        linkedin = {user.links.linkedin}
+        />
+    </div>
+      );
+    }
 }
 
 export default About;
